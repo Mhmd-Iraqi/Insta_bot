be5552a4639f03e64acb21be009a0e4182f559c7
Mhmd-Iraqi <103595056+Mhmd-Iraqi@users.noreply.github.com>
2022-07-16 11:38:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests,user_agent,json,flask,telebot,random,os,sys
-import telebot
+import telebot,darklib
 from telebot import types
 from user_agent import generate_user_agent
 import logging
@@ -11,52 +11,42 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
+@bot.message_handler(commands=["start"])
+def h(message):
+	@bot.message_handler(commands=["sub"])
+	def bmdmf(message):
+		print(k)
+		nkj=len(k)
+		print(nkj)
+		bot.send_message(5009434402,f"اهلا بك مطوري 🙋‍♂️\nعدد مستخدمين بوتك : {nkj}")
+	@bot.message_handler(commands=["listsub"])
+	def jdjdj(message):	
+		bot.send_message(5009434402,f"{k}")
 
-@bot.message_handler(commands=['start'])
-def G(message):
- 
-	mas = types.InlineKeyboardMarkup(row_width=2)
-	A=types.InlineKeyboardButton("CH1",url="https://t.me/WXGDS")
-	B=types.InlineKeyboardButton("CH2",url="https://t.me/WXGDF")
-	C=types.InlineKeyboardButton("Dev🤖",url="https://t.me/WXGDX")
-	mas.add(A,B,C)
-	bot.send_message(message.chat.id,msg,reply_markup=mas,parse_mode="markdown")
-	bot.reply_to(message,text="ID:Sleep", parse_mode = "markdown",disable_web_page_preview="true")
-@bot.message_handler(func=lambda m:True)
+	
+	id = message.chat.id
+	name=message.from_user.first_name
+	username=message.from_user.username
+	if f"username : @{username} id : {id} name : {name}"in k:
+		True
+	else:	
+		k.append(f"username : @{username} id : {id} name : {name}")
+	bot.send_message(id,f"""
+- Welcome [{name}](tg://settings)
 
-def callback(message):
- id = message.chat.id
- while True:
-  try:
-  	if str(id) in str(s):
-  		s.remove(id)
-  		mas = types.InlineKeyboardMarkup(row_width=2)
-  		A=types.InlineKeyboardButton("CH1",url="https://t.me/WXGDS")
-  		B=types.InlineKeyboardButton("CH2",url="https://t.me/WXGDF")
-  		C=types.InlineKeyboardButton("Dev🤖",url="https://t.me/WXGDX")
-  		mas.add(A,B,C)
-  		bot.send_message(message.chat.id,msg,reply_markup=mas,parse_mode="markdown")
-  		bot.reply_to(message,text="ID:Sleep", parse_mode = "markdown",disable_web_page_preview="true")
-  		break
-  		
-  	
-  	ml = message.text
-  	msa = ml.split(":")[0]
-  	sle = ml.split(":")[1]
-  	
-  	url = requests.post(f"https://getcoin-murad-hazem.ols1an.repl.co/?uid={msa}&submit=submit").text
-  
-  	if ('{"coins":') in url:
-  		id = message.chat.id  	
-  		coinx = str(url.split('coins":"')[1])
-  		coin = str(coinx.split('"')[0])
-  		bot.reply_to(message,text=f"""
-  		Coins : {coin}
-""",parse_mode="markdown")
-  		time.sleep(int(sle))
-  except:
-  	bot.reply_to(message,text="Id Blocked")
-  	s.append(id)
+- Programin Bot : [Dark](t.me/j_w_2)
+
+- Please Send username:password""",parse_mode="markdown")
+	@bot.message_handler(func=lambda m:True)
+	def cre(message):
+		id=message.chat.id
+		m=message.text
+		username=m.split(":")[0]
+		password=m.split(":")[1]
+		name = "MyInsta:@4e_m1"
+		sf = Dark.Create(username,password,name)
+		l=f"{sf}"
+		bot.send_message(id,l)
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
